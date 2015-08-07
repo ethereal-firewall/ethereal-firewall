55118d5e993b96f403f4415311625b757af4745b
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-08-07 15:12:35 -0700

    Fixed syntax error in server.js #40

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Sequelize - require('sequelize');
+var Sequelize = require('sequelize');
 
 var db = require('./config/db');
 
