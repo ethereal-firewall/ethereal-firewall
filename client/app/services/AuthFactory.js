52f71763c948f2913c9c6bd1c0233bfb39357e13
halcyonnx <halcyonnx@github.com>
2015-08-07 12:07:30 -0700

    Closed #28 - Added in AuthFactory module

---
+++ client/app/services/AuthFactory.js
@@ -0,0 +1,19 @@
+angular.module('followApp')
+.factory('AuthFactory', function($http) {
+
+  // Signin
+  var signin = function() {
+    return null;
+  };
+
+  // Signup
+  var signup = function() {
+    return null;
+  };
+
+  // Signout
+  var signout = function() {
+    return null;
+  };
+
+});
