d383c373e97a703767148da8bf8ac3557029e866
halcyonnx <halcyonnx@github.com>
2015-08-07 12:20:22 -0700

    Changed ContactsFactory to have all functions defined in single object

--- client/app/services/ContactsFactory.js
+++ client/app/services/ContactsFactory.js
@@ -1,30 +1,29 @@
 angular.module('followApp')
 .factory('ContactsFactory', function($http) {
 
-  // get a single user contact/summary
-  var getContact = function() {
-    return null;
+  var ContactsObj = {
+
+    // get a single user contact/summary
+    getContact : function() {
+      return null;
+    },
+
+    // change a single user contact info
+    setContact : function() {
+      return null;
+    },
+
+    // add a new contact to the user
+    addContact : function() {
+      return null;
+    },
+
+    // get all the user's contacts
+    getContacts : function() {
+      return null;
+    },
+
   };
 
-  // change a single user contact info
-  var setContact = function() {
-    return null;
-  };
-
-  // add a new contact to the user
-  var addContact = function() {
-    return null;
-  };
-
-  // get all the user's contacts
-  var getContacts = function() {
-    return null;
-  };
-
-  return {
-    getContact: getContact,
-    setContact: setContact,
-    addContact: addContact,
-    getContacts: getContacts,
-  };
-})
+  return ContactsObj;
+});
