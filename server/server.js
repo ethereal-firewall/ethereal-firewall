03f53c0ebb733a61273c046d92d63675a3c2800f
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-08-07 14:57:14 -0700

    Fixed client file path in server.js #40

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var Sequelize - require('sequelize');
+
+var db = require('./config/db');
 
 var app = express();
+
+var sequelize = new Sequelize('sequelize', 'root', '', {
+  dialect: 'mysql';
+});
 
 var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/../client'));
 
 app.listen(port);
 console.log('listening on port', port);
