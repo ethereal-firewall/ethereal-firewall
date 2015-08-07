c1959e5ef3d7d297836f6c2a43cc83827d081cbb
halcyonnx <halcyonnx@github.com>
2015-08-07 13:24:03 -0700

    [Refactor] #39 Changed ConversationFactory to have all functions defined in one object.

--- client/app/services/ConversationsFactory.js
+++ client/app/services/ConversationsFactory.js
@@ -1,18 +1,21 @@
 angular.module('followApp')
 .factory('ConversationsFactory', function($http) {
 
-  // Get all the Contact's conversations
-  var getConversations = function() {
-    return null;
+  // Add all functions into this object
+  var ConversationsObj = {
+
+    // Get all the Contact's conversations
+    var getConversations = function() {
+      return null;
+    };
+
+    // Add a conversation to the contact
+    var addConversation = function() {
+      return null;
+    };
+
   };
 
-  // Add a conversation to the contact
-  var addConversation = function() {
-    return null;
-  };
+  return ConversationsObj;
 
-  return {
-    getConversations: getConversations,
-    addConversation: addConversation,
-  }
-})
+});
