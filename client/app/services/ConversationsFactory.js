0e0b465eb22c67ffc3739ce3cf60c43e71a96109
halcyonnx <halcyonnx@github.com>
2015-08-07 14:46:04 -0700

    [Refactor] Fixed #39 changed variable definitions to key, value

--- client/app/services/ConversationsFactory.js
+++ client/app/services/ConversationsFactory.js
@@ -2,20 +2,18 @@ angular.module('followApp')
 .factory('ConversationsFactory', function($http) {
 
   // Add all functions into this object
-  var ConversationsObj = {
+  return  {
 
     // Get all the Contact's conversations
-    var getConversations = function() {
+    getConversations : function() {
       return null;
-    };
+    },
 
     // Add a conversation to the contact
-    var addConversation = function() {
+    addConversation : function() {
       return null;
-    };
+    },
 
   };
-
-  return ConversationsObj;
 
 });
