214a6f9b97c46eed44ade12a0b97207e43b8176c
Darryl Blake <darryl@nikordi.com>
2015-08-10 15:44:56 -0700

    Close #90

--- client/app/services/ContactsFactory.js
+++ client/app/services/ContactsFactory.js
@@ -2,11 +2,38 @@ angular.module('followApp')
 .factory('ContactsFactory', ['$http', function($http) {
 
   return {
-
-    // get a single user contact/summary
     getContact : function() {
-      return null;
+      return new Promise(function(resolve, reject) {
+        resolve(
+          {
+            contact: {
+              firstName: 'Darryl',
+              lastName: 'Blake',
+              phone: '+64 22 450 1100',
+              email: 'darryl@darrylblake.com',
+              interval: 7,
+              nextDate: '2015-08-17 10:37:22',
+            },
+            conversation: [{
+              type: 'E',
+              datetime: '2015-08-17 10:37:22',
+              summary: 'Chocolate bar sweet roll toffee gingerbread.'
+            },
+            {
+              type: 'E',
+              datetime: '2015-08-17 10:37:22',
+              summary: 'Cotton candy cupcake powder sesame snaps. Cotton candy cupcake powder sesame snaps.'
+            },
+            {
+              type: 'E',
+              datetime: '2015-08-17 10:37:22',
+              summary: 'Chocolate bar sweet roll toffee gingerbread.'
+            }],
+          }
+        );
+      });
     },
+      
 
     // change a single user contact info
     setContact : function() {
@@ -20,9 +47,34 @@ angular.module('followApp')
 
     // get all the user's contacts
     getContacts : function() {
-      return null;
+      return new Promise(function(resolve, reject) {
+        resolve(
+          [
+            { firstName: 'Darryl', lastName: 'Blake' },
+            { firstName: 'Lauren', lastName: 'Janicki' },
+            { firstName: 'Brandon', lastName: 'Lewis' },
+            { firstName: 'Xing', lastName: 'Tong' },
+            { firstName: 'Alberto', lastName: 'Lyons' },
+            { firstName: 'Eloise', lastName: 'Santos' },
+            { firstName: 'Evelyn', lastName: 'Brock' },
+            { firstName: 'Doreen', lastName: 'Meyer' },
+            { firstName: 'Olga', lastName: 'Manning' },
+            { firstName: 'Annie', lastName: 'Becker' },
+            { firstName: 'Terri', lastName: 'Wilson' },
+            { firstName: 'Wanda', lastName: 'Pittman' },
+            { firstName: 'Rodolfo', lastName: 'Williams' },
+            { firstName: 'Owen', lastName: 'Gross' },
+            { firstName: 'Andrew', lastName: 'Reynolds' },
+            { firstName: 'Samuel', lastName: 'Bowen' },
+            { firstName: 'Elvira', lastName: 'Weber' },
+            { firstName: 'Casey', lastName: 'Pierce' },
+            { firstName: 'Patti', lastName: 'Richards' },
+            { firstName: 'Shelia', lastName: 'Carlson' },
+            { firstName: 'Danny', lastName: 'Robinson' },
+            { firstName: 'Gwendolyn', lastName: 'Quinn' },
+          ]
+        );
+      });
     },
-
   };
-
 }]);
