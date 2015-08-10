eba0b9f96b6991e6a943ce55e7b7c628fd88f53b
halcyonnx <halcyonnx@github.com>
2015-08-10 15:11:39 -0700

    [Feature] closed #89 - Added agenda.html

--- client/app/route.js
+++ client/app/route.js
@@ -1,13 +1,14 @@
 angular.module('followApp')
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    // signup html and controller
+  $routeProvider
+  .when('/', {
+    // agenda html and controller
   })  
   .when('/signin', {
     // signin html and controller
   })
   .when('/signup', {
-    // agenda html and controller
+    // signup html and controller
   })
   .when('/contacts/:id', {
     // signin html and controller
