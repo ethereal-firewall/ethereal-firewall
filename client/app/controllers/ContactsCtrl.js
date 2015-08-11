7062865a1868279a550847627f7620c26afbb2c0
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-08-11 16:03:55 -0700

    Drafted due date calculator #163

--- client/app/controllers/ContactsCtrl.js
+++ client/app/controllers/ContactsCtrl.js
@@ -9,10 +9,31 @@ angular.module('followApp')
     ContactsFactory.getContacts()
     .then(function(contacts) {
       console.log(contacts);
+
+      // current time as date and in milliseconds:
+      var currentDate = new Date();
+      var currentMSDate = Date.now();
+      // set difference between now and contact due date on each contact
+      contacts.forEach(function(contact) {
+        nextDate = Date.parse(contact.nextDate);
+        contact.due = $scope.timeToContact(nextDate, currentDate, currentMSDate);
+      });
+
       $scope.data.contacts = contacts;
     });
   };
 
   $scope.getContacts();
 
+  $scope.timeToContact = function(nextDate, currentDate, currentMSDate) {
+    // calculate current time in UTC
+    var now = currentMSDate - currentDate.getTimezoneOffset * 60000;
+    // subtract now from current date
+    var dueDate = Math.floor((nextDate - now) / 86400000);
+    // return due date
+    return dueDate;
+  };
+
 }]);
+
+
