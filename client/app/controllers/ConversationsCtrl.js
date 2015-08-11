56b61c857fcee3647d76572a28b6dc7737786815
halcyonnx <halcyonnx@github.com>
2015-08-11 15:26:51 -0700

    [Feature] Closed #153 - Started hooking conversationsCtrl to ConversationFactory

--- client/app/controllers/ConversationsCtrl.js
+++ client/app/controllers/ConversationsCtrl.js
@@ -1,5 +1,22 @@
 angular.module('followApp')
 
-.controller('ConversationsCtrl', ['$scope', function($scope) {
+.controller('ConversationsCtrl', ['$scope', '$rootScope', 'ConversationsFactory', function($scope, $rootScope, ConversationsFactory) {
+
+  $scope.data = {};
+  $scope.data.conversations = [];
+
+  $scope.addConversation = function() {
+    ConversationsFactory.addConversation($scope.conversation)
+    .then(function() {
+
+    });
+  };
+
+  $scope.getConversations = function() {
+    ConverstaionsFactory.getConversations()
+    .then(function(conversations) {
+      $scope.data.conversations = conversations; 
+    });
+  };
   
 }]);
