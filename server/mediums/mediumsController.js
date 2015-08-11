23a29694fb4999c229bc94b25f96ac400010599f
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-08-10 20:12:48 -0700

    Added mediums controller

---
+++ server/mediums/mediumsController.js
@@ -0,0 +1,15 @@
+var utils = require('../config/utils');
+
+module.exports.getMediums = function(req, res) {
+  var models = req.app.get('models');
+  var Medium = models.Medium;
+
+  Medium.sync()
+    .findAll({})
+    .then(function(mediums) {
+      utils.sendResponse(res, 200, mediums);
+    })
+    .catch(function(err) {
+      console.log('Error: ', err);
+    });
+};
