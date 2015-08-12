843c5428f07053ffad96e9ddd1186cb4ca555a9f
Brandon John Lewis <bjlewis88@gmail.com>
2015-08-11 18:17:54 -0700

    implements hashed passwords and session storage

--- server/config/utils.js
+++ server/config/utils.js
@@ -1,5 +1,7 @@
 // utils.js
 
+var bcrypt = require('bcrypt-nodejs');
+
 exports.sendResponse = function (res, statusCode, data) {
   res.status(statusCode);
   res.json(data);
@@ -10,4 +12,25 @@ exports.formatDate = function (date, interval) {
   date = Date.now() + interval * 86400000 - date.getTimezoneOffset() * 60000;
   date = (new Date(date)).toISOString().substring(0, 19).replace('T', ' ');
   return date;
+};
+
+exports.hashPassword = function (password, callback) {
+  bcrypt.genSalt(null, function (err, salt) {
+    bcrypt.hash(password, salt, null, function (err, hash) {
+      callback(hash);
+    });
+  });
+};
+
+exports.comparePassword = function (password, hash, callback) {
+  bcrypt.compare(password, hash, function (err, isMatch) {
+    callback(isMatch);
+  });
+};
+
+exports.createSession = function (req, username, callback) {
+  return req.session.regenerate(function () {
+    req.session.user = username;
+    callback();
+  });
 };
