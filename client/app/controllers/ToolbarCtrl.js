5fb91c67a4a94121e28d49f11d3688717ff21082
Darryl Blake <darryl@nikordi.com>
2015-08-12 09:47:06 -0700

    Trigger focus/blur events on search input field.

--- client/app/controllers/ToolbarCtrl.js
+++ client/app/controllers/ToolbarCtrl.js
@@ -1,5 +1,15 @@
 angular.module('followApp')
 
 .controller('ToolbarCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+  $scope.addContact = function() {
+    console.log('add contact clicked');
+  };
 
+  $scope.searchfocus = function() {
+    console.log('search has focus...');
+  }
+
+  $scope.searchblur = function() {
+    console.log('search lost focus...');
+  }
 }])
