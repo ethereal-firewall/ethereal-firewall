ed48bc4b2196a9eb7bbd3085e49320964dcdaa30
Brandon John Lewis <bjlewis88@gmail.com>
2015-08-11 19:25:18 -0700

    handles requests for /users/signedIn

--- server/users/usersRoutes.js
+++ server/users/usersRoutes.js
@@ -3,5 +3,5 @@ var usersController = require('./usersController.js');
 module.exports = function(app) {
   app.post('/signup', usersController.signup); 
   app.post('/signin', usersController.signin); 
-
+  app.get('/signedin', users.Controller.checkSignedIn);
 };
